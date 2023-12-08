93840a2e90184ccb34a624f4452135d452068ff7
wiem.zayati <wiem.zayati@hydatis.com>
2023-12-08 09:22:30 +0100

    unused import

--- two/src/main/java/com/microservice/two/service/TaskService.java
+++ two/src/main/java/com/microservice/two/service/TaskService.java
@@ -2,8 +2,6 @@ package com.microservice.two.service;
 
 
 import com.microservice.two.domain.TaskDto;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
