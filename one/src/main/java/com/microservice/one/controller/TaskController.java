be53c351ca2b6a2bb14ff4d3991ffe03bc72dbb5
wiem.zayati <wiem.zayati@hydatis.com>
2023-12-08 09:00:08 +0100

    Added property injection for the app2Url field using @Value

--- one/src/main/java/com/microservice/one/controller/TaskController.java
+++ one/src/main/java/com/microservice/one/controller/TaskController.java
@@ -1,5 +1,7 @@
 package com.microservice.one.controller;
 import com.microservice.one.domain.Task;
+import com.microservice.one.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,16 +12,20 @@ public class TaskController {
 
 
 
+    @Value("${api.url-app2}")
+    private String app2Url;
     private final RestTemplate restTemplate;
+    private final TaskRepository taskRepository;
 
-    public TaskController(RestTemplate restTemplate) {
+    public TaskController(RestTemplate restTemplate, TaskRepository taskRepository) {
         this.restTemplate = restTemplate;
+        this.taskRepository = taskRepository;
     }
 
     @PostMapping
     public String createTask(@RequestBody Task task) {
 
-        String app2Url = "http://localhost:8082";
+        taskRepository.save(task);
         restTemplate.postForObject(app2Url + "/receiveTask", task, String.class);
         return "Task sent successfully";
 
--- one/src/main/java/com/microservice/one/service/TaskListener.java
+++ one/src/main/java/com/microservice/one/service/TaskListener.java
@@ -1,15 +1,24 @@
 package com.microservice.one.service;
 
 import com.microservice.one.domain.Task;
+import com.microservice.one.repository.TaskRepository;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TaskListener {
+    private final TaskRepository taskRepository;
 
+    public TaskListener(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
 
     @RabbitListener(queues = "${rabbitmq.queue}")
     public void receiveTask(Task task) {
+        taskRepository.findById(task.getId()).ifPresent(task1 -> {
+            task1.setResult(task.getResult());
+            taskRepository.save(task1);
+        });
         System.out.println("Fake Calculation Result for task " + task.getDescription() + " = " +task.getResult() );
     }
 }
